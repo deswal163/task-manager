26687b7163ad51cc991c250c0a74764ef684972d
deswal163 <shubhamdeswal163@gmail.com>
2024-03-07 06:02:40 +0530

    paginate the tasks. sorts the data acc. to query parameter.

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,17 +18,30 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdBy:asec
 router.get("/tasks", auth, async (req, res) => {
-  const match = {}
-
+  const match = {};
+  const sort = {};
   if (req.query.match) {
-    match.completed = req.query.completed === 'true'
+    match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
     await req.user.populate({
       path: "tasks",
-      match
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
     });
     res.send(req.user.tasks);
   } catch (e) {
